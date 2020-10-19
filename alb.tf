767badca5809e2f911fd9912fe71a5776330638d
abhijit <abhijitcs1417@gmail.com>
2020-10-19 10:27:25 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -28,23 +28,43 @@ resource "aws_lb_listener" "http_redirect" {
   }
 }
 
-/*resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.test.arn
-  port              = "443"
+/*resource "aws_alb_listener" "frontend" {
+  load_balancer_arn = aws_alb.main.id
+  port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.certificate_arn
+  certificate_arn   = aws_acm_certificate.firstopinion_certificate.arn
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.test.arn
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default backend"
+      status_code  = 200
+    }
   }
-}*/
+}
+*/
 
 resource "aws_lb_target_group" "test" {
   name        = "lb-tg-curai-${var.env}-${var.application}"
   port        = var.port
   protocol    = var.protocol
-  target_type = "instance"
+  target_type = "ip"
   vpc_id      = var.vpc_id
+  stickiness {
+    type            = "lb_cookie"
+    cookie_duration = 86400
+    enabled         = true
+  }
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = "/index.html"
+    unhealthy_threshold = "2"
+  }
 }
